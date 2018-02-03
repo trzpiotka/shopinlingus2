94eaf2320e209fa10e65611afa45163d1d5ff35b
trzpiotka <trzpiotka@gmail.com>
2018-02-03 11:23:55 +0100

    Update 2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ class Main {
 
     public static void main(String args[]) {
 
-        Shop stokrotka = new Shop("Stokrotka", "Rejtana 3", new String[]{"spożywczy", "kosmetyczny"});
+        Shop stokrotka = new Shop("Stokrotka", 1, 102, new String[] {"aaa", "bbb"});
+
         Shop rossman = new Shop("Rossman", "Rejtana 3", new String[]{"drogeryjny"});
         Shop him = new Shop("H&M", "Rejtana 3", new String[]{"odzieżowy"});
         Shop orsay = new Shop("Orsay", "Rejtana 3", new String[]{"odzieżowy"});
